d2c118d6f33d76545aea93408548c619e1b551b7
yassine77 <kchaou.yessin@gmail.com>
2022-10-18 12:25:22 +0200

    fe/modify the design of navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,36 +1,30 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark ">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">HPC</a>
+            <a class="navbar-brand" href="#">High Performance Computing</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item">
                     <router-link class="nav-link active" to="/">Home</router-link>
                 </li>
                 <li class="nav-item">
+                    <router-link class="nav-link " to="/about">Services</router-link>
+                </li>
+                <li class="nav-item">
                     <router-link class="nav-link " to="/about">About</router-link>
+                </li>
+                <li class="nav-item">
+
+                    <div @click="$store.dispatch('logout')" class="nav-link ">Logout</div>
                 </li>
            
                 
             </ul>
-            <ul class="navbar-nav me-5 mb-2 mb-lg-0">
-                <li class="nav-item dropdown ">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Profil</a></li>
-                        <!-- <li><router-link class="dropdown-item" to="/logout">Logout</router-link></li> -->
-                        
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <button @click="$store.dispatch('logout')" class="btn navbtn">Logout</button>
-                </li>
-            </ul> 
+
+ 
 
             </div>
         </div>
@@ -47,19 +41,22 @@ export default ({
 </script>
 
 <style lang="scss" >
+:root{
+    --green-color:#bae3c1;
+}
 
 nav{
     background:#001923;
     padding: 40px;
     .navbar-brand{
-        color:#bae3c1;
+        color:var(--green-color);
     }
     .navbar-brand:hover{
-        color:#bae3c1;
+        color:var(--green-color);
     }
 
     .nav-item:hover {
-        background-color: #bae3c1;
+        background-color: var(--green-color);
         transition :all ease-in-out;
     }
     .nav-link ,.nav-link:hover ,.navbtn{
